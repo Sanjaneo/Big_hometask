0a9690d4100e9eff4ea843e655ef50369d395836
sanjaneo <sanjaneo.ak@gmail.com>
2020-12-16 12:59:55 +0300

    final bot

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from menu import create_category_menu
 from resources.bot_messages import TEST_SELECT_CATEGORY, BOT_INFO_MESSAGE, BOT_START_MESSAGE
 
 
+# Send message to user if he write a command "/start" in a chart
 @client.message_handler(commands=["start"])
-def print_info(call):
+def print_start(call):
     client.send_message(call.from_user.id, BOT_START_MESSAGE)
 
 
+# Send message to user if he write a command "/info" in a chart
 @client.message_handler(commands=["info"])
 def print_info(call):
     client.send_message(call.from_user.id, BOT_INFO_MESSAGE)
 
 
+# Send message to user if he write a command "/start_test" in a chart
 @client.message_handler(commands=["start_test"])
 def print_category(message):
     client.send_message(message.from_user.id, TEST_SELECT_CATEGORY, reply_markup=create_category_menu())
@@ -30,4 +33,5 @@ def get_answer(call):
     ClientStorage.get_client(call.from_user).get_answer(call.data, ClientStorage)
 
 
+# This line is needed for the bot always receives messages
 client.polling(none_stop=True, interval=0)
--- resources/bot_messages.py
+++ resources/bot_messages.py
@@ -1,11 +1,13 @@
 BOT_INFO_MESSAGE = "Info"
-BOT_START_MESSAGE = ""
+BOT_START_MESSAGE = f"List of the commands that bot can do:" \
+                    f"\n/info - information about this bot" \
+                    f"\n/start_test - start a test"
 
-TEST_SELECT_CATEGORY = "Выберите тему для прохождения теста:"
+TEST_SELECT_CATEGORY = "Choose the discipline to run the test:"
 
-TEST_PASS_ANSWER = "Верный ответ"
-TEST_FAIL_ANSWER = "Неверный ответ"
+TEST_PASS_ANSWER = "Right answer"
+TEST_FAIL_ANSWER = "The answer is wrong"
 
-TEST_RESULT_MESSAGE = "Тест окончен. Вы набрали {res_count}/{total_count} баллов."
+TEST_RESULT_MESSAGE = "You have completed the test. You get {res_count}/{total_count} points."
 
-QUESTION = "{text} \r\nВыберите правильный вариант ответа:"
+QUESTION = "{text}"
