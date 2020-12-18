2f68eedd3e6d620ef91ec9778f6a5275cb5ddd81
sanjaneo <sanjaneo.ak@gmail.com>
2020-12-18 23:57:44 +0300

    final bot

--- handler.py
+++
@@ -1,8 +0,0 @@
-from main import bot, dp
-
-from aiogram.types import Message
-from config import admin_id
-
-
-async def send_to_admin(dp):
-    await bot.send_message(chat_id=admin_id, text="Бот запущен")
--- resources/bot_messages.py
+++ resources/bot_messages.py
@@ -1,4 +1,6 @@
-BOT_INFO_MESSAGE = "Info"
+BOT_INFO_MESSAGE = "Hi!\n" \
+                   "This is a quiz bot. Here you can find your set of quizzes to be done.\n" \
+                   "Please click /start_test to run a test"
 BOT_START_MESSAGE = f"List of the commands that bot can do:" \
                     f"\n/info - information about this bot" \
                     f"\n/start_test - start a test"
